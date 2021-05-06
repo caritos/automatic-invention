1d0bbee974a5e7f92d139292a9e889e360ca9053
Eladio Caritos <eladio@nightwing.local>
2021-05-06 09:51:23 -0400

    blog working

---
+++ src/test/kotlin/com/caritos/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.caritos.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
